d870286dae656c970dd1ec235182f18463dbd567
Anton Serdyuchenko <anton415@gmail.com>
2024-12-26 09:16:30 +0300

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Anton Serdyuchenko on 26.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
